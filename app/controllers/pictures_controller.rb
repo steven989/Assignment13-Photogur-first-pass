21c54a7071d50653ba4be18a69e022a03ba67d40
Steve Long <steven989@gmail.com>
2014-04-08 13:46:21 -0400

    Making each picture have its own page

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -25,4 +25,47 @@ class PicturesController < ApplicationController
 
     end 
 
+    def picture0
+
+        @pictures = [
+
+            {   name: "A Sunday on La Grande Jatte",
+                year: 1884,
+                artist: "Georges Seurat",
+                url: "http://upload.wikimedia.org/wikipedia/commons/thumb/7/7d/A_Sunday_on_La_Grande_Jatte%2C_Georges_Seurat%2C_1884.jpg/1600px-A_Sunday_on_La_Grande_Jatte%2C_Georges_Seurat%2C_1884.jpg"}
+        ]
+
+    end
+
+    def picture1
+
+        @pictures = [
+
+            
+            {
+                name: "The Scream",
+                year: 1893,
+                artist: "Edvard Munch",
+                url: "http://upload.wikimedia.org/wikipedia/en/thumb/f/f4/The_Scream.jpg/950px-The_Scream.jpg"}
+
+        ]
+
+
+    end
+
+    def picture2
+
+        @pictures = [
+
+            {
+                name: "American Gothic",
+                year: 1930,
+                artist: "Grant Wood",
+                url: "http://allthingsmundane.files.wordpress.com/2010/03/american-gothic.jpg"}
+
+        ]
+
+
+    end
+
 end 
